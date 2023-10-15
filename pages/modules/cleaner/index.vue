1087e6b9c4c0b6103af326dfda06a53cf99711bf
Fernando lópez <fernanlm88@gmail.com>
2023-10-15 21:40:36 +0200

    Updated Cleaner

--- pages/modules/cleaner/index.vue
+++ pages/modules/cleaner/index.vue
@@ -1,71 +1,57 @@
 <script setup lang="ts">
-const DIRT_KWS = ref(``)
-const CLEAN_KWS = ref("")
-const REPLACE_ACCENTS = ref(true)
-const REMOVE_BIG = ref(true)
-const REMOVE_CHARACTERS = ref(true)
+const DIRT_KWS = ref(``);
+const CLEAN_KWS = ref("");
+const REPLACE_ACCENTS = ref(true);
+const REMOVE_BIG = ref(true);
+const REMOVE_CHARACTERS = ref(true);
+const SET_UNIQUES = ref(true);
 
 const onClickButtonClear = () => {
-  CLEAN_KWS.value = DIRT_KWS.value
+  CLEAN_KWS.value = DIRT_KWS.value;
   if (REPLACE_ACCENTS) {
-    CLEAN_KWS.value = CLEAN_KWS.value.split('Á').join('a')
-    CLEAN_KWS.value = CLEAN_KWS.value.split('á').join('a')
-    CLEAN_KWS.value = CLEAN_KWS.value.split('É').join('e')
-    CLEAN_KWS.value = CLEAN_KWS.value.split('é').join('e')
-    CLEAN_KWS.value = CLEAN_KWS.value.split('Í').join('i')
-    CLEAN_KWS.value = CLEAN_KWS.value.split('í').join('i')
-    CLEAN_KWS.value = CLEAN_KWS.value.split('Ó').join('o')
-    CLEAN_KWS.value = CLEAN_KWS.value.split('ó').join('o')
-    CLEAN_KWS.value = CLEAN_KWS.value.split('Ú').join('u')
-    CLEAN_KWS.value = CLEAN_KWS.value.split('ú').join('u')
-    CLEAN_KWS.value = CLEAN_KWS.value.split('Ñ').join('n')
-    CLEAN_KWS.value = CLEAN_KWS.value.split('ñ').join('n')
+    CLEAN_KWS.value = CLEAN_KWS.value.split("Á").join("a");
+    CLEAN_KWS.value = CLEAN_KWS.value.split("á").join("a");
+    CLEAN_KWS.value = CLEAN_KWS.value.split("É").join("e");
+    CLEAN_KWS.value = CLEAN_KWS.value.split("é").join("e");
+    CLEAN_KWS.value = CLEAN_KWS.value.split("Í").join("i");
+    CLEAN_KWS.value = CLEAN_KWS.value.split("í").join("i");
+    CLEAN_KWS.value = CLEAN_KWS.value.split("Ó").join("o");
+    CLEAN_KWS.value = CLEAN_KWS.value.split("ó").join("o");
+    CLEAN_KWS.value = CLEAN_KWS.value.split("Ú").join("u");
+    CLEAN_KWS.value = CLEAN_KWS.value.split("ú").join("u");
+    CLEAN_KWS.value = CLEAN_KWS.value.split("Ñ").join("n");
+    CLEAN_KWS.value = CLEAN_KWS.value.split("ñ").join("n");
   }
 
   if (REMOVE_CHARACTERS) {
     const regex = /[^a-zA-Z0-9\n ]/g;
-    CLEAN_KWS.value = CLEAN_KWS.value.replace(regex, '');
+    CLEAN_KWS.value = CLEAN_KWS.value.replace(regex, "");
   }
 
   if (REMOVE_BIG) {
-    CLEAN_KWS.value = CLEAN_KWS.value.split('\n').filter(line => line.split(' ').length <= 10).join('\n');
+    CLEAN_KWS.value = CLEAN_KWS.value
+      .split("\n")
+      .filter((line) => line.split(" ").length <= 10)
+      .join("\n");
   }
-
-}
-
+  if(SET_UNIQUES){
+    CLEAN_KWS.value = [... new Set(CLEAN_KWS.value.split("\n"))].join("\n");
+  }
+};
 </script>
 <template>
-  <div class="grid grid-cols-3 gap-2">
-    <div class="flex flex-col">
-      <label for="dirt">Copy here your dirt keywords</label>
-      <textarea id="dirt" rows="19" placeholder="Here your keywords" v-model="DIRT_KWS"
-        class=" border-2 border-gray-300 dark:bg-slate-400 disabled:cursor-nSot-allowed disabled:opacity-50  p-4 rounded-xl w-full" />
-    </div>
-    <div class="flex flex-col gap-">
+  <div class="grid grid-cols-3 gap-2 mt-4">
+    <UTextarea :rows="19" placeholder="Insert here the keywords list" label="Copy here the dirt keywords"
+      v-model="DIRT_KWS" />
+    <div class="flex flex-col gap-4">
       <h2 class="text-2xl text-center">FILTERS</h2>
-      <div class="flex gap-2">
-        <input type="checkbox" id="dirt" v-model="REPLACE_ACCENTS" />
-        <label for="dirt">Replaced accented letters (á, é, í..) for not accented letters (a, e, i...)</label>
-      </div>
-      <div class="flex gap-2">
-        <input type="checkbox" id="dirt" v-model="REMOVE_BIG" />
-        <label for="dirt">Remove long tail keywords with more of 10 words</label>
-      </div>
-      <div class="flex gap-2">
-        <input type="checkbox" id="dirt" v-model="REMOVE_CHARACTERS" />
-        <label for="dirt">Remove unwished characters like + - </label>
-      </div>
-      <div class="flex gap-2">
-        <Button type="Button"
-          class="py-2 px-4  bg-indigo-600 hover:bg-indigo-600 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg"
-          @click="onClickButtonClear()">Clear</Button>
-      </div>
+      <UCheckbox label="Replace accented letters (á, é, í....) for not accented letters (a, e, i...)" v-model="REPLACE_ACCENTS" />
+      <UCheckbox label="Remove long tail keywords with more of 10 words" v-model="REMOVE_BIG" />
+      <UCheckbox label="Remove unwished characters like + -" v-model="REMOVE_CHARACTERS" />
+      <UCheckbox label="Remove duplicated content" v-model="SET_UNIQUES" />
+      <UButton label="Clear" @click="onClickButtonClear()" />
     </div>
+    <UTextarea :rows="19" label="Your cleaned keywords" v-model="CLEAN_KWS" readonly/>
 
-    <div class="flex flex-col">
-      <label for="clean">Your cleaned keywords</label>
-      <textarea id="clean" rows="19" placeholder="Here your keywords" v-model="CLEAN_KWS" readonly
-        class=" border-2 border-gray-300 dark:bg-slate-400 disabled:cursor-nSot-allowed disabled:opacity-50  p-4 rounded-xl w-full" />
-    </div>
   </div>
 </template>
