b5b0eb735000b51794caa2579314619788a4b828
Fernando lópez <fernanlm88@gmail.com>
2023-10-29 08:55:18 +0100

    changes to vuestic

--- pages/cleaner.vue
+++ pages/cleaner.vue
@@ -34,25 +34,32 @@ const onClickButtonClear = () => {
       .filter((line) => line.split(" ").length <= 10)
       .join("\n");
   }
-  if(SET_UNIQUES){
+  if (SET_UNIQUES) {
     CLEAN_KWS.value = [... new Set(CLEAN_KWS.value.split("\n"))].join("\n");
   }
 };
 </script>
 <template>
-  <div class="grid grid-cols-3 gap-2">
-    <textarea rows="20" v-model="CLEAN_KWS" label="Insert here the keywords list" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+  <va-card class="h-full">
+    <va-card-content>
+      <div class="grid grid-cols-3 gap-2">
+        <va-textarea rows="20" v-model="CLEAN_KWS" label="Insert here the keywords list"
+          class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
 
-    <div class="flex flex-col gap-4">
-      <h2 class="text-2xl text-center">FILTERS</h2>
-      <CheckboxComponent label="Replace accented letters (á, é, í....) for not accented letters (a, e, i...)" v-model="REPLACE_ACCENTS" />
-      <CheckboxComponent label="Remove long tail keywords with more of 10 words" v-model="REMOVE_BIG" />
-      <CheckboxComponent label="Remove unwished characters like + -" v-model="REMOVE_CHARACTERS" />
-      <CheckboxComponent label="Remove duplicated content" v-model="SET_UNIQUES" />
-      <button type="button"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-32"
-        @click="onClickButtonClear()">Clear keywords</button>
-    </div>
-    <textarea rows="20" v-model="CLEAN_KWS" label="Your cleaned keywords" readonly  class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
-  </div>
-</template>
+        <div class="flex flex-col gap-4">
+          <h2 class="text-2xl text-center">FILTERS</h2>
+          <va-checkbox label="Replace accented letters (á, é, í....) for not accented letters (a, e, i...)"
+            v-model="REPLACE_ACCENTS" />
+          <va-checkbox label="Remove long tail keywords with more of 10 words" v-model="REMOVE_BIG" />
+          <va-checkbox label="Remove unwished characters like + -" v-model="REMOVE_CHARACTERS" />
+          <va-checkbox label="Remove duplicated content" v-model="SET_UNIQUES" />
+          <button type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-32"
+            @click="onClickButtonClear()">Clear keywords</button>
+        </div>
+        <va-textarea rows="20" v-model="CLEAN_KWS" label="Your cleaned keywords" readonly
+          class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+      </div>
+
+    </va-card-content>
+  </va-card></template>
--- pages/project/index.vue
+++ pages/project/index.vue
@@ -46,8 +46,7 @@ const onClickButtonSaveProject = () => {
     const export_data = {
         project: localStorage.getItem('project'),
         keywords: JSON.parse(localStorage.getItem('keywords') || ''),
-        structure: JSON.parse(localStorage.getItem('structure') || ''),
-        blog: JSON.parse(localStorage.getItem('blog') || '')
+        structure: JSON.parse(localStorage.getItem('structure') || '')
     }
 
     const file = new Blob([JSON.stringify(export_data)], { type: 'text/json' });
@@ -66,7 +65,6 @@ const onInputFileLoadProject = async (event: any) => {
     localStorage.setItem('keywords', JSON.stringify(ImportedData.keywords))
     localStorage.setItem('project', ImportedData.project)
     localStorage.setItem('structure', ImportedData.structure)
-    localStorage.setItem('blog', ImportedData.blog)
     localStorage.keywords = text
     location.reload();
 };
@@ -75,7 +73,6 @@ const onInputFileLoadCSV = async (event: any) => {
     localStorage.removeItem('keywords')
     localStorage.removeItem('project')
     localStorage.removeItem('structure')
-    localStorage.removeItem('blog')
     const file = event.target.files[0];
     const text = await file.text();
     const lines = text.split('\n');
@@ -154,12 +151,12 @@ const onInputFileLoadCSV = async (event: any) => {
     localStorage.setItem('project', INPUT_PROJECT_NAME.value)
     localStorage.setItem('keywords', JSON.stringify(data))
     localStorage.setItem('structure', JSON.stringify([]))
-    localStorage.setItem('blog', JSON.stringify([]))
     // location.reload();
 };
 
 const onInputSelectPreset = (event: any) => {
-    if (event.target.value === 'Google') {
+    console.log(event)
+    if (event === 'Google') {
         INPUT_NUMBER_STARTING_LINE.value = 6
         INPUT_KEYWORD_COLUMN.value = 1
         INPUT_VOLUME_COLUMN.value = 4
@@ -172,45 +169,41 @@ const onInputSelectPreset = (event: any) => {
 </script>
 <template>
     <NuxtLayout name="projects">
-        <h2 class="va-h2">Current project: {{ projectName }}</h2>
-        <!--Loader-->
-        <article class="grid grid-cols-2 gap-2 w-full">
-            <section class="flex flex-col items-center justify-center">
-                <button type="button"
-                    class="disabled:opacity-50 disabled:cursor-not-allowed text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                    @click="onClickButtonSaveProject()" :disabled="disabledButtonSaveProject">
-                    Save the project</button>
-                <button type="button"
-                    class="disabled:opacity-50 disabled:cursor-not-allowed text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                    @click="onClickButtonSaveProject()" :disabled="disabledButtonSaveProject">
-                    Export to Excel</button>
-                <p v-if="disabledButtonSaveProject">No projects loaded in your browser</p>
+        <va-card>
+            <va-card-title>
+                <h2 class="va-h4">Current project: {{ !disabledButtonSaveProject ? projectName : 'No project loaded' }}</h2>
+            </va-card-title>
+            <va-card-content>
+                <article class="grid grid-cols-2 gap-2 w-full">
+                    <section class="flex flex-col items-center justify-center gap-4">
+                        <va-button type="button" @click="onClickButtonSaveProject()" :disabled="disabledButtonSaveProject">
+                            Save the project</va-button>
+                        <va-button type="button" @click="onClickButtonSaveProject()" :disabled="disabledButtonSaveProject">
+                            Export to Excel</va-button>
+                        <p v-if="disabledButtonSaveProject">No projects loaded in your browser</p>
 
-            </section>
-            <section>
-                <div class="p-4 my-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
-                    role="alert" v-if="!disabledButtonSaveProject">
-                    <span class="font-medium">Note: </span> Load a project removes the current active
-                    project, please save it before load other one.
-                </div>
-                <label for="dropzone-file"
-                    class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                    <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                        <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-                        </svg>
-                        <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Load project,
-                                click
-                                here </span> or drag and drop</p>
-                    </div>
-                    <input id="dropzone-file" type="file" class="hidden" accept=".json"
-                        @input="onInputFileLoadProject($event)" />
-                </label>
-            </section>
-        </article>
-        <h2 class="border-b font-bold mb-6 pb-4 text-2xl  w-full">New project</h2>
+                    </section>
+                    <section>
+                        <va-alert color="#ce6e67" class="mb-6" v-if="!disabledButtonSaveProject">
+                            <span class="font-medium">Note: </span> Load a project removes the current active
+                            project, please save it before load other one.
+                        </va-alert>
+
+                        <va-file-upload dropzone @file-added="onInputFileLoadProject($event)" />
+                    </section>
+                </article>
+
+            </va-card-content>
+        </va-card>
+        <va-card>
+            <va-card-title>
+                <h2 class="va-h4">New project</h2>
+            </va-card-title>
+            <va-card-content>
+
+            </va-card-content>
+        </va-card>
+        <!--Loader-->
         <!--Loader-->
         <article class="grid grid-cols-2 gap-4 w-full">
             <section class="col-span-2">
@@ -223,105 +216,22 @@ const onInputSelectPreset = (event: any) => {
                 </div>
             </section>
             <section class="grid grid-cols-2 gap-6 mb-6">
-                <div>
-                    <label for="project_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        Project name *
-                    </label>
-                    <input type="text" id="project_name" v-model="INPUT_PROJECT_NAME"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-                <div>
-                    <label for="presets"
-                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Presets</label>
-                    <select id="presets" v-model="INPUT_SELECT_PRESET" @input="onInputSelectPreset($event)"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                        <option value="None">None</option>
-                        <option value="Google">Google Keyword Planner</option>
-                    </select>
-                </div>
-                <div>
-                    <label for="separator" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Separator
-                        *</label>
-                    <select id="separator" v-model="INPUT_SELECT_SEPARATOR"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                        <option value="Tabulations">Tabulations</option>
-                        <option value="Comma">Comma</option>
-                        <option value="Semicolon">Semicolon</option>
-                        <option value="Spaces">Spaces</option>
-                    </select>
-                </div>
-                <div>
-                    <label for="Headers_line" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        Starting line *</label>
-                    <input type="number" id="Headers_line" v-model="INPUT_NUMBER_STARTING_LINE"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-                <div>
-                    <label for="keyword_column" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        Keyword column *
-                    </label>
-                    <input type="number" id="keyword_column" v-model="INPUT_KEYWORD_COLUMN"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-                <div>
-                    <label for="volume_column" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        Volume column *
-                    </label>
-                    <input type="number" id="keyword_column" v-model="INPUT_VOLUME_COLUMN"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-                <div>
-                    <label for="volume_column" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        Google Ads High range
-                    </label>
-                    <input type="number" id="keyword_column" v-model="INPUT_ADS_HIGH"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-                <div>
-                    <label for="volume_column" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        Dirt keywords filter (that you like remove from kw research)
-                    </label>
-
-
-                    <textarea id="message" rows="4" v-model="TEXTAREA_BLACKLIST"
-                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Write the blacklist keyword comma separated, example: brand1, unnecesary, text..."></textarea>
-                </div>
-                <div>
-                    <label for="volume_column" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        Informational filter
-                    </label>
-                    <textarea id="message" rows="4" v-model="TEXTAREA_FILTER_INFORMATIONAL"
-                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Write the informational of keywords comma separated, example: best, new, discover..."></textarea>
-                </div>
-                <div>
-                    <label for="volume_column" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                        Transactional filter
-                    </label>
-                    <textarea id="message" rows="4" v-model="TEXTAREA_FILTER_TRANSACTIONAL"
-                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="Write the transactional of keywords comma separated, example: price, purchase, shop..."></textarea>
-                </div>
-
+                <va-input type="text" v-model="INPUT_PROJECT_NAME" label="Project name *" />
+                <va-select v-model="INPUT_SELECT_PRESET" class="col-span-1" label="Presets"
+                    :options="['None', 'Google']" highlight-matched-text @Update:modelValue="onInputSelectPreset($event)"  />
+                <va-select v-model="INPUT_SELECT_SEPARATOR" class="col-span-1" label="CSV separator *"
+                    :options="['Tabulations', 'Comma','Semicolon','Spaces']" highlight-matched-text />
+                
+                <va-input type="number" v-model="INPUT_NUMBER_STARTING_LINE" label="Starting line *" />
+                <va-input type="number" v-model="INPUT_KEYWORD_COLUMN" label="Keyword column *" />
+                <va-input type="number" id="keyword_column" v-model="INPUT_VOLUME_COLUMN" label="Volume column *" />
+                <va-input type="number" id="keyword_column" v-model="INPUT_ADS_HIGH" label="Google Ads High range" />
+                <va-textarea label="Black list" placeholder="All keywords that contain words listed here will be removed" v-model="TEXTAREA_BLACKLIST" />
+                <va-textarea label="Informational list" placeholder="All keywords that contain words listed here will set as Informational" v-model="TEXTAREA_FILTER_INFORMATIONAL" />
+                <va-textarea label="Transactional list" placeholder="All keywords that contain words listed here will set as Transactional" v-model="TEXTAREA_FILTER_TRANSACTIONAL" />
             </section>
             <section>
-                <label for="dropzone-file-csv"
-                    class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg bg-gray-50  dark:bg-gray-700  dark:border-gray-600 "
-                    :class="disabledCSVUploader ? 'opacity-50 cursor-not-allowed' : 'dark:hover:bg-bray-800 cursor-pointer hover:bg-gray-100 dark:hover:border-gray-500 dark:hover:bg-gray-600'">
-                    <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                        <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-                        </svg>
-                        <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Load CSV file,
-                                click
-                                here </span> or drag and drop</p>
-                    </div>
-                    <input id="dropzone-file-csv" type="file" class="hidden" accept=".csv" :disabled="disabledCSVUploader"
-                        @input="onInputFileLoadCSV($event)" />
-                </label>
+                <va-file-upload dropzone @file-added="onInputFileLoadCSV($event)" :disabled="disabledCSVUploader" />
             </section>
         </article>
     </NuxtLayout>
