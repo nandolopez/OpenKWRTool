8c3a52a1dc08377dfc238296435961528f6cd85e
Fernando lópez <fernanlm88@gmail.com>
2023-11-01 21:14:29 +0100

    added title to page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   app: {
     baseURL: "/OpenKWRTool",
-    buildAssetsDir: 'assets'
+    buildAssetsDir: 'assets',
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title:"Open source Keyword research tool"
+    }
   },
   nitro: { output: { publicDir: "../nandolopez.github.io/OpenKWRTool" } },
 
